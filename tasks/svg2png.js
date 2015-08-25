c568120429218e2e5a5e3e1e77b36ee70a9ec35b
Morgan Estes <morgan.estes@gmail.com>
2015-08-25 16:22:51 -0500

    Enabled destination path and file name changes for scaled images.

--- tasks/svg2png.js
+++ tasks/svg2png.js
@@ -1,36 +1,33 @@
+/* global require:false */
+var path = require( "path" );
+var async = require( "async" );
+var svg2png = require( "svg2png" );
+var chalk = require( "chalk" );
+var numCPUs = require( "os" ).cpus().length;
 
-"use strict";
+module.exports = function ( grunt ) {
+	"use strict";
 
-var path = require("path");
-var async = require("async");
-var svg2png = require("svg2png");
-var chalk = require("chalk");
-var numCPUs = require("os").cpus().length;
+	grunt.registerMultiTask( "svg2png", "Convert SVG to PNG", function () {
+		var options = this.options( {
+			scale: 1.0,
+			suffix: '',
+			limit: Math.max( numCPUs, 2 )
+		} );
 
-module.exports = function (grunt) {
+		async.eachLimit( this.files, options.limit, function ( el, next ) {
+			var pngFileName = path.basename( el.src, ".svg" ) + options.suffix.toString() + ".png";
+			var destinationFile = path.join( el.orig.dest, pngFileName );
 
-  grunt.registerMultiTask("svg2png", "Convert SVG to PNG", function () {
-    var options = this.options({
-      scale: 1.0,
-      subdir: "",
-      limit: Math.max(numCPUs, 2)
-    });
-
-    async.eachLimit(this.files, options.limit, function (el, next) {
-      var rootdir = path.dirname(el.src);
-      var pngFile = path.basename(el.src, ".svg") + ".png";
-      var dest = path.join(rootdir, options.subdir, pngFile);
-
-      svg2png(el.src, dest, options.scale, function (err) {
-        if (err) {
-          grunt.log.error("An error occurred converting %s in %s: %s", el.src, dest, err);
-        }
-        else {
-          grunt.log.writeln(chalk.green("✔ ") + dest + chalk.gray(" (scale:", options.scale + ")"));
-        }
-        next();
-      });
-    }, this.async());
-  });
-
+			svg2png( el.src, destinationFile, options.scale, function ( err ) {
+				if ( err ) {
+					grunt.log.error( "An error occurred converting %s in %s: %s", el.src, destinationFile, err );
+				}
+				else {
+					grunt.log.writeln( chalk.green( "✔ " ) + destinationFile + chalk.gray( " (scale:", options.scale + ")" ) );
+				}
+				next();
+			} );
+		}, this.async() );
+	} );
 };
